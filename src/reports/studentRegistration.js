753ca7cbdc4e5b5274b363615900873eb5316d5e
ankit12332 <ankit.panda010.ap@gmail.com>
2023-05-14 17:01:05 +0530

    edited printing

--- src/reports/studentRegistration.js
+++ src/reports/studentRegistration.js
@@ -4,34 +4,50 @@ import { PDFViewer, Document, Page, Text, StyleSheet, View } from '@react-pdf/re
 const styles = StyleSheet.create({
   page: {
     padding: 30,
+    fontFamily: 'Helvetica',
   },
   title: {
     fontSize: 24,
     textAlign: 'center',
     marginBottom: 20,
+    fontWeight: 'bold',
+    textDecoration: 'underline',
   },
-  header: {
-    fontSize: 18,
-    marginBottom: 30,
-    textAlign: 'left',
+  section: {
+    marginBottom: 20,
   },
   field: {
     flexDirection: 'row',
     marginBottom: 10,
   },
   fieldTitle: {
-    width: '35%',
+    width: '40%',
     fontSize: 14,
     fontWeight: 'bold',
   },
   fieldValue: {
-    width: '65%',
+    width: '60%',
     fontSize: 14,
+  },
+  header: {
+    marginBottom: 20,
+  },
+  headerTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
   },
   line: {
     borderBottomColor: '#000000',
     borderBottomWidth: 1,
     marginBottom: 10,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 30,
+    left: 30,
+    right: 30,
+    textAlign: 'center',
+    fontSize: 10,
   },
 });
 
@@ -40,40 +56,47 @@ const StudentRegistration = ({ studentData }) => {
     <PDFViewer width="100%" height="100%">
       <Document>
         <Page style={styles.page}>
-          <Text style={styles.title}>Student Profile</Text>
-          <View style={styles.line} />
+          <View style={styles.section}>
+            <Text style={styles.title}>Student Registration</Text>
+            <View style={styles.line} />
+          </View>
           <Text style={styles.header}>Student Information</Text>
-          <View style={styles.field}>
-            <Text style={styles.fieldTitle}>Student Name:</Text>
-            <Text style={styles.fieldValue}>{studentData.studentName}</Text>
+          <View style={styles.section}>
+            <View style={styles.field}>
+              <Text style={styles.fieldTitle}>Student Name:</Text>
+              <Text style={styles.fieldValue}>{studentData.studentName}</Text>
+            </View>
+            <View style={styles.field}>
+              <Text style={styles.fieldTitle}>Email:</Text>
+              <Text style={styles.fieldValue}>{studentData.email}</Text>
+            </View>
+            <View style={styles.field}>
+              <Text style={styles.fieldTitle}>Father's Name:</Text>
+              <Text style={styles.fieldValue}>{studentData.fatherName}</Text>
+            </View>
+            <View style={styles.field}>
+              <Text style={styles.fieldTitle}>Mother's Name:</Text>
+              <Text style={styles.fieldValue}>{studentData.motherName}</Text>
+            </View>
+            <View style={styles.field}>
+              <Text style={styles.fieldTitle}>Gender:</Text>
+              <Text style={styles.fieldValue}>{studentData.gender}</Text>
+            </View>
+            <View style={styles.field}>
+              <Text style={styles.fieldTitle}>Date of Birth:</Text>
+              <Text style={styles.fieldValue}>{studentData.dateOfBirth}</Text>
+            </View>
+            <View style={styles.field}>
+              <Text style={styles.fieldTitle}>Department:</Text>
+              <Text style={styles.fieldValue}>{studentData.department}</Text>
+            </View>
+            <View style={styles.field}>
+              <Text style={styles.fieldTitle}>Mobile Number:</Text>
+              <Text style={styles.fieldValue}>{studentData.mobileNumber}</Text>
+            </View>
           </View>
-          <View style={styles.field}>
-            <Text style={styles.fieldTitle}>Email:</Text>
-            <Text style={styles.fieldValue}>{studentData.email}</Text>
-          </View>
-          <View style={styles.field}>
-            <Text style={styles.fieldTitle}>Father's Name:</Text>
-            <Text style={styles.fieldValue}>{studentData.fatherName}</Text>
-          </View>
-          <View style={styles.field}>
-            <Text style={styles.fieldTitle}>Mother's Name:</Text>
-            <Text style={styles.fieldValue}>{studentData.motherName}</Text>
-          </View>
-          <View style={styles.field}>
-            <Text style={styles.fieldTitle}>Gender:</Text>
-            <Text style={styles.fieldValue}>{studentData.gender}</Text>
-          </View>
-          <View style={styles.field}>
-            <Text style={styles.fieldTitle}>Date of Birth:</Text>
-            <Text style={styles.fieldValue}>{studentData.dateOfBirth}</Text>
-          </View>
-          <View style={styles.field}>
-            <Text style={styles.fieldTitle}>Department:</Text>
-            <Text style={styles.fieldValue}>{studentData.department}</Text>
-          </View>
-          <View style={styles.field}>
-            <Text style={styles.fieldTitle}>Mobile Number:</Text>
-            <Text style={styles.fieldValue}>{studentData.mobileNumber}</Text>
+          <View style={styles.footer}>
+            <Text>Generated on: {new Date().toLocaleDateString()}</Text>
           </View>
         </Page>
       </Document>
@@ -81,4 +104,4 @@ const StudentRegistration = ({ studentData }) => {
   );
 };
 
-export default StudentRegistration;
+export default StudentRegistration
